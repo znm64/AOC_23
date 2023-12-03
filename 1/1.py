b5463230f4847ad73ce26cfd98483ce234192260
Zubin M <102535933+znm64@users.noreply.github.com>
2023-12-03 23:06:02 +0000

    minor tweak

--- 1/1.py
+++ 1/1.py
@@ -3,8 +3,8 @@
 debugreg = []
 lines = open('data.txt', 'r').readlines()
 line_nums = [[] for i in range(len(lines))]
-#originally, the mappin of words to num was one:1, but for part 2, i changed the mapping  to also include the first and last letters
-#note that twone should evaluate to 21, not 2ne, in part 2. This was not mentioned in the instuctions, and does not affect the test case
+#originally, the mapping of words to num was one:1, but for part 2, i changed the mapping to also include the first and last letters
+#this was because twone should evaluate to 21, not 2ne, in part 2. This was not mentioned in the instuctions, and does not affect the test case
 #this wouln't make a difference if you were only searching for the values, but i chose to replace the values and then read for digits(not the best way to do it) 
 map = [['o1e', "one"], ['t2o', "two"], ['t3e', "three"], ['f4r', "four"], ['f5e', "five"], ['s6x', "six"], ['s7n', "seven"], ['e8t', "eight"], ['n9e', "nine"]]
 for linenum, i in enumerate(lines):
