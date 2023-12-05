505d209578f45236fd3ed8779d7160a7e51fb57c
Zubin M <102535933+znm64@users.noreply.github.com>
2023-12-05 20:48:21 +0000

    day 5 part 1

--- 5/5.py
+++ 5/5.py
@@ -1,9 +1,6 @@
 #part 1
 raw = [i for i in open('5data.txt', 'r').readlines()]
-print(repr(raw[0]))
-#raw[0] = raw[0].replace('seeds:', 'seeds:\n')
 raw.insert(1, raw[0][7:])
-print(raw)
 processed = []
 current = []
 for i in raw:
@@ -13,9 +10,6 @@ for i in raw:
         processed.append(current)
         current = []
 processed.append(current)
-
-print(processed)
-
 processed_2 = []
 for count, i in enumerate(processed):
     current = []
@@ -23,31 +17,23 @@ for count, i in enumerate(processed):
         i2 = i2.strip()
         current.append(i2.split(' '))
     processed_2.append(current[:-1])
-
-print(processed_2)
-
+    
 seeds = processed_2[1]
 mappings = processed_2[2:]
 newseeds = []
 seeds = [int(i) for i in seeds[0]]
-print('seeds', seeds)
 mappings = [[[int(i) for i in j] for j in k] for k in mappings]
-print('mappings', mappings)
 x = [[0 for i in range(20)] for i in range(8)]
 #for each seed
 for count, seed in enumerate(seeds):
-    testtrigger = (seed == 14)
     #for each mapping type
     for mapnum, fullmap in enumerate(mappings):
-        print('newmap', mapnum)
         changemade = False
+        #for each individual map triplet
         for i in fullmap:
             if i[1] <= seed and seed <= i[1]+i[2]+1 and not changemade:
                 seed = seed - i[1]+i[0]
-                print('changed')
                 changemade = True
-                if testtrigger:
-                    print(seed)
     seeds[count] = seed
 
 print(min(seeds))
